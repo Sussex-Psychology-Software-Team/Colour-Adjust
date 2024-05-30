c12189d3bba9390b938b7b50b895a37b37a61152
max-lovell <max_lovell@hotmail.co.uk>
2024-05-30 10:59:59 +0100

    Install android prompt

--- colour-adjust.js
+++ colour-adjust.js
@@ -202,4 +202,21 @@ function removeY(){
 }
 
 drawLAB(50)
-let installPrompt = null;const installButton = document.getElementById("install");window.addEventListener("beforeinstallprompt", (event) => {    event.preventDefault()    installPrompt = event    installButton.removeAttribute("hidden")})installButton.addEventListener("click", async () => {    if (!installPrompt) { return}    const result = await installPrompt.prompt()    console.log(`Install prompt was: ${result.outcome}`)    installPrompt = null    installButton.setAttribute("hidden", "")});
+
+
+let installPrompt = null;
+const installButton = document.getElementById("install");
+
+window.addEventListener("beforeinstallprompt", (event) => {
+    event.preventDefault()
+    installPrompt = event
+    installButton.removeAttribute("hidden")
+})
+
+installButton.addEventListener("click", async () => {
+    if (!installPrompt) { return}
+    const result = await installPrompt.prompt()
+    console.log(`Install prompt was: ${result.outcome}`)
+    installPrompt = null
+    installButton.setAttribute("hidden", "")
+});
