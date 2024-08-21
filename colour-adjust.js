ad8c2c4e44fb966ad8d688779a72dfb8d892d11d
max-lovell <max_lovell@hotmail.co.uk>
2024-08-21 16:24:57 +0100

    Move LAB Clamp function down

--- colour-adjust.js
+++ colour-adjust.js
@@ -178,19 +178,6 @@ function lch2lab(lch){
     return lab
 }
 
-function clampLAB(lab){
-    // L* 0-100
-    if(lab.l<0) lab.l=0
-    else if(lab.l>100) lab.l=100
-    // a*-127-128
-    if(lab.a<-127) lab.a=-127
-    else if(lab.a>128) lab.a=128
-    //b* -127-128
-    if(lab.b<-127) lab.b=-127
-    else if(lab.b>128) lab.b=128
-    return { 'l':lab.l, 'a':lab.a, 'b':lab.b }
-}
-
 // Wrappers
 // Convert RGB to LAB
 function rgb2lab(rgb){
@@ -289,6 +276,19 @@ function sameColour(colour1, colour2, channels){
     return colour1[channels.charAt(0)] === colour2[channels.charAt(0)] &&
     colour1[channels.charAt(1)] === colour2[channels.charAt(1)] &&
     colour1[channels.charAt(2)] === colour2[channels.charAt(2)]
+}
+
+function clampLAB(lab){
+    // L* 0-100
+    if(lab.l<0) lab.l=0
+    else if(lab.l>100) lab.l=100
+    // a*-127-128
+    if(lab.a<-127) lab.a=-127
+    else if(lab.a>128) lab.a=128
+    //b* -127-128
+    if(lab.b<-127) lab.b=-127
+    else if(lab.b>128) lab.b=128
+    return { 'l':lab.l, 'a':lab.a, 'b':lab.b }
 }
 // mod fuction to handle negative numbers
 function mod(n, m) { //https://stackoverflow.com/questions/4467539/javascript-modulo-gives-a-negative-result-for-negative-numbers
