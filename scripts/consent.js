0dbbb50e7a012a293cc6d3b0755e85fa8829d8c1
max-lovell <max_lovell@hotmail.co.uk>
2024-09-19 09:54:22 +0100

    add fullscreen button to consent

--- scripts/consent.js
+++ scripts/consent.js
@@ -33,3 +33,35 @@ function submitConsent(e){
     document.getElementById('confirmSettingsPage').hidden = false;
     currentPage = 'confirmSettingsPage';
 }
+
+document.getElementById('fullscreen').addEventListener('click',(e)=>{
+    if(isFullscreen()) {
+        if (document.exitFullscreen) {
+          document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+          document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+          document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) {
+          document.msExitFullscreen();
+        }
+      } else {
+        element = document.documentElement;
+        if (element.requestFullscreen) {
+          element.requestFullscreen();
+        } else if (element.mozRequestFullScreen) {
+          element.mozRequestFullScreen();
+        } else if (element.webkitRequestFullscreen) {
+          element.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+        } else if (element.msRequestFullscreen) {
+          element.msRequestFullscreen();
+        }
+      }
+})
+
+function isFullscreen(){
+    return document.fullscreenElement ||
+    document.webkitFullscreenElement ||
+    document.mozFullScreenElement ||
+    document.msFullscreenElement
+}
