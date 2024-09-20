fa4e4543d4c22c62fd5a89cf13b99b8ebce32cf9
max-lovell <max_lovell@hotmail.co.uk>
2024-09-20 17:14:25 +0100

    Add fullscreen button to settings page

--- scripts/confirm_settings.js
+++ scripts/confirm_settings.js
@@ -6,3 +6,35 @@ function confirmSettings(e){
     // Setup trials and call new trial function
     setupTrials()
 }
+
+document.getElementById('fullscreen').addEventListener('click',(e)=>{
+    if(isFullscreen()) {
+        if (document.exitFullscreen) {
+          document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+          document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+          document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) {
+          document.msExitFullscreen();
+        }
+      } else {
+        element = document.documentElement;
+        if (element.requestFullscreen) {
+          element.requestFullscreen();
+        } else if (element.mozRequestFullScreen) {
+          element.mozRequestFullScreen();
+        } else if (element.webkitRequestFullscreen) {
+          element.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+        } else if (element.msRequestFullscreen) {
+          element.msRequestFullscreen();
+        }
+      }
+  })
+  
+  function isFullscreen(){
+      return document.fullscreenElement ||
+      document.webkitFullscreenElement ||
+      document.mozFullScreenElement ||
+      document.msFullscreenElement
+  }
